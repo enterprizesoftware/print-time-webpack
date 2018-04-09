d98ec7d6f1406bc2e54ee23ad8270cbf8217ded9
Jamel Toms <jameltoms@gmail.com>
2018-04-08 23:07:28 -0700

    Update API for Webpack 4

--- print-time-webpack-plugin.js
+++ print-time-webpack-plugin.js
@@ -1,17 +1,26 @@
 const chalk = require('chalk');
 
-function PrintTimeWebpackPlugin() {}
+function PrintTimeWebpackPlugin(options) {
+}
 
-PrintTimeWebpackPlugin.prototype.apply = function(compiler) {
-  compiler.hooks.watchRun.tap('PrintTimeWebpackPlugin', function(compiler, callback) {
+PrintTimeWebpackPlugin.prototype.apply = function (compiler) {
+  (
+    compiler.hooks ?
+      compiler.hooks.watchRun.tapAsync.bind(compiler.hooks.watchRun, 'PrintTimeWebpackPlugin') :
+      compiler.plugin.bind(compiler, 'watch-run')
+  )(function (compilation, callback) {
     let separator = '';
     for (let i = 0; i < 80; i++) separator += '_';
     console.log('\n' + separator);
 
     callback();
   });
-  compiler.hooks.done.tap('PrintTimeWebpackPlugin', function() {
-    setTimeout(function() {
+  (
+    compiler.hooks ?
+      compiler.hooks.done.tap.bind(compiler.hooks.done, 'PrintTimeWebpackPlugin') :
+      compiler.plugin.bind(compiler, 'done')
+  )(function () {
+    setTimeout(function () {
       console.log(chalk.cyan(message()));
     }, 100);
   });
